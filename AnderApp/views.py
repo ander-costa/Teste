6e39a3e1f44cc31d9f20b77c70d36ecc8b831940
Anderson Costa <ander.costa@outlook.com>
2019-12-11 10:02:06 -0300

    Vai

--- AnderApp/views.py
+++ AnderApp/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 
 def index(request):
-    return HttpResponse("Olá Anderson")
+    now = datetime.now()
+
+    html_content = "<html><head><title>Olá, Anderson</title></head><body>"
+    html_content += "<strong>Olá Anderson</strong> on " + now.strftime("%A, %d %B, %Y at %X")
+    html_content += "</body></html>"
+
+    return HttpResponse(html_content)
 
 # Create your views here.
